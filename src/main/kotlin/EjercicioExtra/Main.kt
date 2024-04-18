40cd5de369ae649b9e895111252dc372dcc707ba
dpazbar29 <145806563+dpazbar29@users.noreply.github.com>
2024-04-18 18:31:40 +0200

    Ejercicio Extra

---
+++ src/main/kotlin/EjercicioExtra/Main.kt
@@ -0,0 +1,44 @@
+package EjercicioExtra
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+@Suppress("ktlint:standard:function-naming")
+@Composable
+@Preview
+fun App() {
+    var count by remember { mutableStateOf(0) }
+    var primerClic by remember { mutableStateOf(true) }
+
+    MaterialTheme {
+        Column {
+            Button(onClick = {
+                count++
+                primerClic = false
+            }) {
+                Text(if (primerClic) "Empezar" else "Contador: $count")
+            }
+
+            Button(onClick = { count = 0 }) {
+                Text("Resetear")
+            }
+        }
+    }
+}
+
+fun main() =
+    application {
+        Window(onCloseRequest = ::exitApplication) {
+            App()
+        }
+    }
