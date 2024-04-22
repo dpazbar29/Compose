80a8fb29cd3024383e4962203279bf03ffaad0b5
dpazbar29 <145806563+dpazbar29@users.noreply.github.com>
2024-04-22 19:08:21 +0200

    TrayTwo y Menu Bar

---
+++ src/main/kotlin/menuTrayNotifications/TrayTwo.kt
@@ -0,0 +1,29 @@
+package menuTrayNotifications
+
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.window.Tray
+import androidx.compose.ui.window.application
+
+fun main() =
+    application {
+        Tray(
+            icon = TrayIconTwo,
+            menu = {
+                Item(
+                    "Exit",
+                    onClick = ::exitApplication,
+                )
+            },
+        )
+    }
+
+object TrayIconTwo : Painter() {
+    override val intrinsicSize = Size(256f, 256f)
+
+    override fun DrawScope.onDraw() {
+        drawOval(Color(0xFFFFA500))
+    }
+}
