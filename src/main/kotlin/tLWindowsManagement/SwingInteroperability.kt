97f0ea5eabe20d7489429a97586047e36323d93f
dpazbar29 <145806563+dpazbar29@users.noreply.github.com>
2024-04-22 18:41:26 +0200

    Swing Interoperability

---
+++ src/main/kotlin/tLWindowsManagement/SwingInteroperability.kt
@@ -0,0 +1,18 @@
+package tLWindowsManagement
+
+import androidx.compose.ui.awt.ComposeWindow
+import java.awt.Dimension
+import javax.swing.JFrame
+import javax.swing.SwingUtilities
+
+fun main() =
+    SwingUtilities.invokeLater {
+        ComposeWindow().apply {
+            size = Dimension(300, 300)
+            defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE
+            setContent {
+                // Content
+            }
+            isVisible = true
+        }
+    }
